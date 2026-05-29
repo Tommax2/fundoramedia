7b5426cdba99cd9d78aa19b49775cbe51ab3874d
Tommax2 <martinsolumi@gmail.com>
2026-05-29 16:00:52 +0100

    addd

--- src/pages/BlogPostPage.jsx
+++ src/pages/BlogPostPage.jsx
@@ -178,7 +178,7 @@ function BlogPostPage() {
                     <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
                   </svg>
                   {likeCount > 0 ? <span>{likeCount}</span> : null}
-                  <span>{liked ? "Liked" : "Like"}</span>
+                  <span>{liked ? "Unlike" : "Like"}</span>
                 </button>
               </div>
 
